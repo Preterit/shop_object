91ae26de58de8f0a35e5b1608e1e199b0d17bd6c
lwb <18614005205@163.com>
2020-04-02 06:55:31 +0800

    域名动态获取。实现

--- app/src/main/java/com/shangyi/business/ui/splash/model/SplashModel.kt
+++ app/src/main/java/com/shangyi/business/ui/splash/model/SplashModel.kt
@@ -2,15 +2,10 @@ package com.shangyi.business.ui.splash.model
 
 import android.util.Log
 import com.sdxxtop.base.BaseViewModel
-import com.shangyi.business.net.APIServer
-import com.shangyi.business.net.Constom
-import com.shangyi.kt.CustomCallAdapterFactory
-import com.shangyi.kt.CustomGsonConverterFactory
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.shangyi.kt.NetInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 
 
 /**
@@ -20,31 +15,22 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 class SplashModel : BaseViewModel() {
     fun loadSettingInfo() {
-//        showLoadingDialog(true)
-//        loadOnUI({
-//            val params = Params()
-//            RetrofitClient.apiServicePort.postGetSetting()
-//        }, {
-//            mIsLoadingShow.value = false
-//            Log.e("SplashModel -- ", "${it.toString()}")
-//        }, { code, msg, t ->
-//            UIUtils.showToast(msg)
-//            mIsLoadingShow.value = false
-//        })
-        val retrofit = Retrofit.Builder()
-                .baseUrl(Constom.GETNETWORK_PORT)
-                .addConverterFactory(CustomGsonConverterFactory.create())
-                .addCallAdapterFactory(CustomCallAdapterFactory.creat())
+        val url = "http://39.106.156.132/server"
+        val okHttpClient = OkHttpClient().newBuilder().addInterceptor(NetInterceptor()).build()
+        val request: Request = Request.Builder()
+                .url(url)
+                .get() //默认就是GET请求，可以不写
                 .build()
-        val httpService: APIServer = retrofit.create(APIServer::class.java)
-        val cookCall = httpService.postGetSetting()
-        cookCall.enqueue(object : Callback<Any> {
-            override fun onFailure(call: Call<Any>, t: Throwable) {
-                Log.e("SplashModel --- ", "onFailure ---- ${t.message}")
+        val newCall = okHttpClient.newCall(request)
+        newCall.enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                Log.e("SplashModel --1-- ", "onFailure: ")
             }
 
-            override fun onResponse(call: Call<Any>, response: Response<Any>) {
-                Log.e("SplashModel --- ", "onResponse ---- ${response.body().toString()}")
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                var responseBody = response.body()
+                Log.e("SplashModel --2-- ", "onResponse: ${responseBody?.string()}")
+                responseBody?.close()
             }
 
         })
