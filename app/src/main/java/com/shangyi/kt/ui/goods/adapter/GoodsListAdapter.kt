7ac9f8c735e85529c131284f106d8df55f65aae3
lwb <18614005205@163.com>
2020-04-10 18:11:19 +0800

    商品详情添加跳转路由

--- app/src/main/java/com/shangyi/kt/ui/goods/adapter/GoodsListAdapter.kt
+++ app/src/main/java/com/shangyi/kt/ui/goods/adapter/GoodsListAdapter.kt
@@ -1,8 +1,10 @@
 package com.shangyi.kt.ui.goods.adapter
 
+import android.content.Intent
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.shangyi.business.R
+import com.shangyi.kt.ui.goods.GoodsDetailActivity
 
 /**
  * Date:2020/4/10
@@ -17,6 +19,8 @@ class GoodsListAdapter : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_
     }
 
     override fun convert(holder: BaseViewHolder, item: String) {
-
+        holder.itemView.setOnClickListener {
+            context.startActivity(Intent(context, GoodsDetailActivity::class.java))
+        }
     }
 }
