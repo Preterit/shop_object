1c426bc4c3c8df4bb1dcc11f69efacecad42e598
dz <13793014727@163.com>
2020-04-15 17:50:42 +0800

    优化设置页面

---
+++ app/src/main/java/com/shangyi/kt/ui/personal/SettingActivity.java
@@ -0,0 +1,19 @@
+package com.shangyi.kt.ui.personal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shangyi.business.R;
+
+/**
+ * 设置页面
+ */
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
